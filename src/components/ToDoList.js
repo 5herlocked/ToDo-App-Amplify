b9102189668877c50bf68d45beef36fd0c384168
Shardul Vaidya <cam.v737@gmail.com>
2021-09-23 23:48:48 -0400

    Submission commit

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -5,13 +5,19 @@ import ToDoItem from "./ToDoItem";
 import "./ToDoList.css";
 import {Button} from "@mui/material";
 import {SortOutlined} from "@mui/icons-material"
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 
 const ToDoList = ({todos, onRemove}) => {
     const [localToDo, setlocalToDo] = useState(todos);
-    const [sortMethod, setSortMethod] = useState('title');
+    const [sortMethod, setSortMethod] = useState(null);
+    const [menuView, setMenuView] = useState(false);
 
     useEffect(() => {
         const sortArray = method => {
+            if (sortMethod == null || localToDo.length <= 0) {
+                return;
+            }
             const methods = {
                 title: 'title',
                 description: 'description',
@@ -38,9 +44,38 @@ const ToDoList = ({todos, onRemove}) => {
         }
     }
 
+    const handleMenu = (event) => {
+        setMenuView(event.currentTarget);
+    }
+
+    const handleClose = () => {
+        setMenuView(null);
+    }
+
     return (
         <div>
-            <Button variant={"outlined"} endIcon={<SortOutlined/>} onChange={}/>
+            <div>
+                <Button variant={"outlined"} onClick={handleMenu} endIcon={<SortOutlined/>}>Sort By</Button>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={menuView}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(menuView)}
+                    onClose={handleClose}>
+                    <MenuItem onClick={() => setSortMethod('title')}>Title</MenuItem>
+                    <MenuItem onClick={() => setSortMethod('description')}>Description</MenuItem>
+                    <MenuItem onClick={() => setSortMethod('status')}>Status</MenuItem>
+                    <MenuItem onClick={() => setSortMethod('dueDate')}>Due Date</MenuItem>
+                </Menu>
+            </div>
             <div>
                 {
                     localToDo.map((todo, index) => (
